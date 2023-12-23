caf2b37fb57320c3a5b710ac12c8cf96e3761d45
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-23 00:03:42 -0800

    user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -4,6 +4,7 @@
 """
 import hashlib
 import uuid
+import unittest
 
 
 class User():
@@ -57,8 +58,7 @@ class User():
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
-if __name__ == '__main__':
-    print("Test User")
+class TestUser(unittest.TestCase):
 
     user_1 = User()
     if user_1.id is None:
@@ -85,10 +85,12 @@ if __name__ == '__main__':
         print("User.password should be None if setter to an integer")
 
     if user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right password")
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right password")
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
         print("is_valid_password should return False if compare with None")
@@ -97,4 +99,9 @@ if __name__ == '__main__':
         print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password set before")
+        print("is_valid_password should return False if no password set \
+before")
+        
+if __name__ == '__main__':
+    print("Test User")
+    unittest.main()
