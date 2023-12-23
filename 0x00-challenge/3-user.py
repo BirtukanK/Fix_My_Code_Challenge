3fcbdd9341d04af1f3c599b1b09a08db0c5c35af
Birtukan Kuma <birtukankuma1113@gmail.com>
2023-12-23 00:08:09 -0800

    user

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -4,7 +4,6 @@
 """
 import hashlib
 import uuid
-import unittest
 
 
 class User():
@@ -41,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -58,7 +57,8 @@ class User():
         return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
 
 
-class TestUser(unittest.TestCase):
+if __name__ == '__main__':
+    print("Test User")
 
     user_1 = User()
     if user_1.id is None:
@@ -84,7 +84,7 @@ class TestUser(unittest.TestCase):
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if user_1.is_valid_password(u_pwd):
+    if not user_1.is_valid_password(u_pwd):
         print("is_valid_password should return True if it's the right \
 password")
 
@@ -101,7 +101,3 @@ password")
     if user_2.is_valid_password("No pwd"):
         print("is_valid_password should return False if no password set \
 before")
-        
-if __name__ == '__main__':
-    print("Test User")
-    unittest.main()
